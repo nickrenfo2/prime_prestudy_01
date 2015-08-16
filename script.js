fe20a0d69d8db78a3656ce481da80651150c65be
nickrenfo2 <nickrenfo@gmail.com>
2015-08-16 17:25:13 -0500

    Add math functions for fun math mysteries!

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 
     console.log("Hey, this works!");
+
+function mathA(x) {
+    return x*x;
+}
+function mathB(x) {
+    return x+2;
+}
+function mathC(x) {
+    return x*2
+}
+
+console.log(mathA(mathB(mathC(3))));
